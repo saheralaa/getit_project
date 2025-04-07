4981bb77314aaece9d50303988cb1ad0ced8503d
saher alaa <151808466+saheralaa@users.noreply.github.com>
2025-04-07 13:03:10 +0200

    ssss

--- sheardp/lib/screens/home_screen.dart
+++ sheardp/lib/screens/home_screen.dart
@@ -35,7 +35,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   context,
                   MaterialPageRoute(builder: (context) => const LoginScreen()),
                 );
-              
               },
               child: Text('Log out'),
             ),
